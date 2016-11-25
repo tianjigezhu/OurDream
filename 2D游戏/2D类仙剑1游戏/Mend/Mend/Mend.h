644b81b60c1471296217ba0d745803ddf9428015
tianjigezhu <tianjigezhuhn@aliyun.com>
2016-11-26 00:31:22 +0800

    添加了游戏界面文档内容。 源代码中添加了按钮部分代码。

--- 2D游戏/2D类仙剑1游戏/Mend/Mend/Mend.h
+++ 2D游戏/2D类仙剑1游戏/Mend/Mend/Mend.h
@@ -3,10 +3,14 @@
 
 namespace Mend
 {
-	//
+	// 所用类型定义命名空间
 	namespace MendTypes
+	{
+	}
+	// 所用UI元素命名空间
+	namespace UI
 	{
 	}
 }
 
-#endif
+#endif	
---
+++ 2D游戏/2D类仙剑1游戏/Mend/Mend/button.h
@@ -0,0 +1,16 @@
+#ifndef _UI_BUTTON_H_
+#define _UI_BUTTON_H_
+
+namespace Mend
+{
+	namespace UI
+	{
+		//
+		class CButton
+		{
+
+		};
+	}
+}
+
+#endif
