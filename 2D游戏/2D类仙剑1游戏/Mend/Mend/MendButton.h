221e21755af9b567534d6535183b1ebc60d371d1
tianjigezhu <tianjigezhuhn@aliyun.com>
2016-11-26 21:12:23 +0800

    修改部分代码，UML添加了部分内容，文档中添加了部分内容。

---
+++ 2D游戏/2D类仙剑1游戏/Mend/Mend/MendButton.h
@@ -0,0 +1,16 @@
+#ifndef _UI_MEND_BUTTON_H_
+#define _UI_MEND_BUTTON_H_
+
+namespace Mend
+{
+	namespace UI
+	{
+		//
+		class CMendButton
+		{
+
+		};
+	}
+}
+
+#endif
---
+++ 2D游戏/2D类仙剑1游戏/Mend/Mend/MendMath.h
@@ -0,0 +1,15 @@
+#ifndef _MEND_MATH_H_
+#define _MEND_MATH_H_
+
+namespace Mend
+{
+	namespace MendMath
+	{
+		struct MendPoint
+		{
+			int x, y;
+		};
+	}
+}
+
+#endif
